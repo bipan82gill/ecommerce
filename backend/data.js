c271dd135847549a9bbc3ae871be223d7b653b5f
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-11-24 16:25:53 -0500

    create user Schema and Route

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
+
 const data ={
+    users:[
+        {
+            name:"Bipan",
+            email:"bipan82.ca@gmail.com",
+            password: bcrypt.hashAsync('1234',8),
+            isAdmin:true,
+        },
+        {   name:"user",
+            email:"user@gmail.com",
+            password: bcrypt.hashAsync('1234',8),
+            isAdmin:false,
+        }
+    ],
     products:[
         {
             _id:'1',
